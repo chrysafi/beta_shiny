05571c02a9159c9752831133ab22f557e5513d32
Anna Chrysafi <chrysafi@users.noreply.github.com>
2015-04-10 15:43:08 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,65 +1,53 @@
 # Define server logic required to draw figure
 ###set workign directory to run the app###
 
-#setwd("C:/Users/chrysafi/")
+setwd("C:/Users/chrysafi/")
 
 library(shiny)
 
 shinyServer(
   function(input, output, session) {
     
-      
-
-alphaconv<-function(m,sd)
-(
-  m*(((m*(1-m))/(sd^2))-1)
-  ) 
-  
-betaconv<-function(m,sd)
-(
-  (1-m)*(((m*(1-m))/(sd^2))-1) 
-)
-
-output$Betaplot <- renderPlot(
+##########beta distribution#############    
+    
+    alphaconv<-function(m,sd)
+      (
+        m*(((m*(1-m))/(sd^2))-1)
+      ) 
+    
+    betaconv<-function(m,sd)
+      (
+        (1-m)*(((m*(1-m))/(sd^2))-1) 
+      )
+    
+    output$Betaplot <- renderPlot(
 {
   t<-seq(0,1,0.001)
   Dist.out<-dbeta(t,alphaconv(input$m,input$sd),betaconv(input$m,input$sd))
   # plot beta
-  plot(t,Dist.out, col = rgb(0/255,86/255,149/255),main="Beta for dataset a",xlab="Depletion",ylab="probability",xlim=c(0,1),ylim=c(0,5),type="l",lwd=5)
+  plot(t,Dist.out, col = rgb(0/255,86/255,149/255),main="Beta for distribution for stock depletion",xlab="Depletion",ylab="probability",xlim=c(0,1),ylim=c(0,5),type="l",lwd=5)
 }
-)
+    )
 
-output$Betaplot2 <- renderPlot(
-{
-  t<-seq(0,1,0.001)
-  Dist.out<-dbeta(t,alphaconv(input$m2,input$sd2),betaconv(input$m2,input$sd2))
-  # plot beta
-  plot(t,Dist.out, col = rgb(0/255,86/255,149/255),main="Beta for dataset b",xlab="Depletion",ylab="probability",xlim=c(0,1),ylim=c(0,5),type="l",lwd=5)
-}
-)
-output$Betaplot3 <- renderPlot(
-{
-  t<-seq(0,1,0.001)
-  Dist.out<-dbeta(t,alphaconv(input$m3,input$sd3),betaconv(input$m3,input$sd3))
-  # plot beta
-  plot(t,Dist.out, col = rgb(0/255,86/255,149/255),main="Beta for dataset c",xlab="Depletion",ylab="probability",xlim=c(0,1),ylim=c(0,5),type="l",lwd=5)
-}
-)
-output$Betaplot4 <- renderPlot(
-{
-  t<-seq(0,1,0.001)
-  Dist.out<-dbeta(t,alphaconv(input$m4,input$sd4),betaconv(input$m4,input$sd4))
-  # plot beta
-  plot(t,Dist.out, col = rgb(0/255,86/255,149/255),main="Beta for dataset d",xlab="Depletion",ylab="probability",xlim=c(0,1),ylim=c(0,5),type="l",lwd=5)
-}
-)
+##########reactive elements#############
 
 a<-reactive({
-  as.numeric(c(input$select,input$m,input$sd,input$m2,input$sd2,input$m3,input$sd3,input$m4,input$sd4))
+  as.numeric(c(paste(input$select,input$dataset,sep=""),input$m,input$sd))
 })
 
+
+
+b<-reactive({
+  as.numeric(paste(input$select,input$dataset,sep=""))
+})
+
+d<-reactive({
+  as.numeric(input$select)
+})
+
+##############reactive table##############
 values<- reactiveValues()
-values$df<-data.frame(Species=numeric(0),mu_a=numeric(0),sd_a=numeric(0),mu_b=numeric(0),sd_b=numeric(0),mu_c=numeric(0),sd_c=numeric(0),mu_d=numeric(0),sd_d=numeric(0))
+values$df<-data.frame(Species_Scenario=numeric(0),mean=numeric(0),std=numeric(0))
 newEntry<- observe({
   if(input$update >0){
     newLine<-isolate(a())
@@ -70,7 +58,7 @@ newEntry<- observe({
 output$mytable<-renderTable({values$df})
 
 
-####downloading data
+####downloading data#############
 
 output$downloadData <- downloadHandler(
   filename = function() {
@@ -80,10 +68,108 @@ output$downloadData <- downloadHandler(
     write.csv(values$df, file)
   },contentType="csv"
 )
+####image file#################
+
+output$plot<-renderUI({
+  
+      img(src=paste("FISH",b(),".png",sep=""), height = 600, width = 600)
+})
+  
+#####information provided##########  
+output$info <- renderText({
+  
+  if ( d()==1 ) {
+    info <- "Family:Gadidae VBF K: 0.2 Linf: 50 cm Amax: 30 yrs Lmat: 20 cm Management started: 1995"
+  } else {
+    
+    if ( d()==2)  {
+  info <- "Family:Scombridae VBF K: 0.3 Linf: 60 cm Amax: 10 Lmat: 25 cm Management started: 1990"
+    } else {
+      
+      if ( d()==3 ) {
+        info <- "family:C VBF K:Linf:Amax:Lmat:Management started:"
+      } else {
+        
+        if ( d()==4 ) {
+          info <- "family:D VBF K:Linf:Amax:Lmat:Management started:"
+        } else {
+          
+          if ( d()==5) {
+            info <- "family:E VBF K:Linf:Amax:Lmat:Management started:"
+          } else { 
+            
+            if ( d()==6 ) {
+              info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+            }
+            else {
+              
+              if ( d()==7)  {
+                info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+              } else {
+                
+                if ( d()==8 ) {
+                  info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                } else {
+                  
+                  if ( d()==9 ) {
+                    info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                  } else {
+                    
+                    if ( d()==10) {
+                      info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                    } else { 
+                      
+                      if ( d()==11 ) {
+                        info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                      }
+                      else {
+                        
+                        if ( d()==12)  {
+                          info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                        } else {
+                          
+                          if ( d()==13 ) {
+                            info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                          } else {
+                            
+                            if ( d()==14 ) {
+                              info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                            } else {
+                              
+                              if ( d()==15) {
+                                info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                              } else { 
+                                
+                                if ( d()==16 ) {
+                                  info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                                } else {
+                                  
+                                  if ( d()==17)  {
+                                    info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                                  } else {
+                                    
+                                    if ( d()==18 ) {
+                                      info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                                    } else {
+                                      
+                                      if ( d()==19 ) {
+                                        info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                                      } else {
+                                        
+                                        if ( d()==20) {
+                                          info <- "family:VBF K:Linf:Amax:Lmat:Management started:"
+                                        } 
+          }
+        }
+      }
+    }
+  }
+                            }}}}}}}}}}}}}}
+  
+}) 
 
 
 
 
   }
 )
-
