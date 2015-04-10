98503e700a79ef342afce3149af1f30e61223797
Anna Chrysafi <chrysafi@users.noreply.github.com>
2015-04-10 15:43:36 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ library(shiny)
 shinyUI(fluidPage(
   
   #  Application title
-  titlePanel("Beta distribution for depletion prior"),
+  titlePanel("Prior for stock depletion"),
   
   # Sidebar with sliders that demonstrate various available
   # options
@@ -15,62 +15,55 @@ shinyUI(fluidPage(
     sidebarPanel
     (
       
-        column(6,sliderInput("m", "Mean dataset a", 
-                  min=0, max=1, value=0.4)),    
+            
       
-      
-      column(6,sliderInput("sd", "Sigma dataset a", 
-                 min = 0, max = 0.5, value = 0.2)),
-      column(6,sliderInput("m2", "Mean dataset b", 
-                  min=0, max=1, value=0.4)),    
-      
-      
-      column(6,sliderInput("sd2", "Sigma dataset b", 
-                  min = 0, max = 0.5, value = 0.2)),
-      column(6,sliderInput("m3", "Mean dataset c", 
-                  min=0, max=1, value=0.4)),    
-      
-      
-      column(6,sliderInput("sd3", "Sigma dataset c", 
-                  min = 0, max = 0.5, value = 0.2)),
-      column(6,sliderInput("m4", "Mean dataset d", 
-                           min=0, max=1, value=0.4)),    
-      
-      
-      column(6,sliderInput("sd4", "Sigma dataset d", 
-                           min = 0, max = 0.5, value = 0.2)),
-    
-      column(10,
-             selectInput("select", label = h3("Select species"), 
+             selectInput("select", label = h4("Select species"), 
                          choices = list("Species 1" = 1, "Species 2" = 2,
                                         "Species 3" = 3,"Species 4"=4, "Species 5" = 5,
-                         "Species 6" = 6,"Species 7" = 7,"Species 8" = 8, "Species 9" = 9,
-                         "Species 10" = 10,"Species 11" = 11, "Species 12" = 12,
-                         "Species 13" = 13, "Species 14" = 14, "Species 15" = 15,
-                         "Species 16" = 16,"Species 17" = 17, "Species 18" = 18, "Species 19" = 19,
-                         "Species 20" = 20),selected = 1)),
-      column(10,actionButton("update", "Update Table"))
+                                        "Species 6" = 6,"Species 7" = 7,"Species 8" = 8, "Species 9" = 9,
+                                        "Species 10" = 10,"Species 11" = 11, "Species 12" = 12,
+                                        "Species 13" = 13, "Species 14" = 14, "Species 15" = 15,
+                                        "Species 16" = 16,"Species 17" = 17, "Species 18" = 18, "Species 19" = 19,
+                                        "Species 20" = 20),selected = 1),
+     selectInput("dataset",label=h4("select data set"),
+                            choices=list("Data set a"=1,"Data set b"=2,"Data set c"=3,"data set d"=4),
+                            selected=1),
+    h4("Scroll the bars to adjust depletion"),
+     column(6,sliderInput("m", "Mean ", 
+                          min=0, max=1, value=0.4)),    
      
+     
+     column(6,sliderInput("sd", "Sigma ", 
+                          min = 0, max = 0.5, value = 0.2)),
+     
+      
+      actionButton("update", "Update Table"),
+     
+     column(12,h3("Table for depletion")),
+     
+     column(10,tableOutput("mytable"))
+      
       
       
     ),
     
     # Plot beta distribution and equation
     mainPanel(
+      h4("Background info for the species:"),
+      h5(textOutput("info")),
+      uiOutput("plot"),
       
-      column(6,plotOutput("Betaplot")),
-      column(6,plotOutput("Betaplot2")),
-      column(6,plotOutput("Betaplot3")),
-      column(6,plotOutput("Betaplot4")),
+      plotOutput("Betaplot"),
+      
       tags$head( tags$style( HTML('#mytable table {border-collapse:collapse; } 
                              #mytable table th { transform: rotate(-45deg)}'))),
-      tableOutput("mytable"),
+      
       
       downloadLink('downloadData', 'Download')  
-    
       
-      )
+      
+    )
     
     
-  
-)))
+    
+  )))
